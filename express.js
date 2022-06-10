4c241c3cb2376192eaf7e5c89cbb7e2c73a0da40
satikghazaryan <webtrainersatik@gmail.com>
2022-06-10 12:28:59 +0400

    edit express.js

--- express.js
+++ express.js
@@ -1,12 +1,16 @@
 let express = require("express");
-const path = require("path");
+//const path = require("path");
 const app = express();
 app.listen(8000);
-app.use((req, res, next) => {
-  next();
-});
+// app.use((req, res, next) => {
+//   next();
+// });
 app.use(express.static("public"));
+// app.get("/", (req, res) => {
+//   // res.send("hello NodeJs");
+//   res.sendFile(path.resolve("public/index.html"));
+// });
+
 app.get("/", (req, res) => {
-  // res.send("hello NodeJs");
-  res.sendFile(path.resolve("public/index.html"));
+  req.redirect("/index.html");
 });
